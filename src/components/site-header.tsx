6e66e3d56bd88cd201d98b2da935b38e096658f0
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-04-28 23:16:04 -0300

    feat: update authentication url callback

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/ui/separator"
 import { Button } from "./ui/button"
 
 export function SiteHeader() {
-  const authenticationUrl = "https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=4933724664056622&redirect_uri=http://apiteste-j2za.onrender.com/callback"
+  const authenticationUrl = "https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=4933724664056622&redirect_uri=https://junkes00.github.io/dash-test/"
 
   return (
     <header className="flex h-12 shrink-0 items-center gap-2 border-b ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
